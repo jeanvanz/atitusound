3caede41b1ce9a12eae0440455dc6fc48033d488
Jean Folle Vanz <jeanfolle86@gmail.com>
2023-11-19 18:43:22 -0300

    Aula 18
    Criada toda a estrutura para o CRUD de Músicas

---
+++ src/main/java/br/edu/atitus/poo/atitusound/dtos/DTOOnlyUuid.java
@@ -0,0 +1,17 @@
+package br.edu.atitus.poo.atitusound.dtos;
+
+import java.util.UUID;
+
+public class DTOOnlyUuid {
+
+	private UUID uuid;
+
+	public UUID getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(UUID uuid) {
+		this.uuid = uuid;
+	}
+	
+}
---
+++ src/main/java/br/edu/atitus/poo/atitusound/repositories/MusicRepository.java
@@ -0,0 +1,10 @@
+package br.edu.atitus.poo.atitusound.repositories;
+
+import org.springframework.stereotype.Repository;
+
+import br.edu.atitus.poo.atitusound.entities.MusicEntity;
+
+@Repository
+public interface MusicRepository extends GenericRepository<MusicEntity>{
+
+}
