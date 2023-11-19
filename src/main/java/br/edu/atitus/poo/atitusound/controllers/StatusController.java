b707cc8f8e7c872321a9c4e39295427f3583a595
Jean Folle Vanz <jeanfolle86@gmail.com>
2023-11-19 13:43:32 -0300

    Aula 2
    Primeira Controller e Mapeamento de métodos HTTP

---
+++ src/main/java/br/edu/atitus/poo/atitusound/controllers/StatusController.java
@@ -0,0 +1,25 @@
+package br.edu.atitus.poo.atitusound.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("status")
+public class StatusController {
+
+	@GetMapping({"","/aplicacao"})
+	public String getStatus() {
+		return "Aplicação está no Ar!";
+	}
+	
+	@PostMapping
+	public ResponseEntity<String> postStatus(@RequestBody String textoEntrada) {
+		System.out.println(textoEntrada);
+		return ResponseEntity.status(HttpStatus.CREATED).body("Retorno Método POST - Sucesso");
+	}
+}
