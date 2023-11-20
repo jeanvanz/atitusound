ae14d64d3b2c9da0f9566be7f7d58161d468d3ea
Jean Folle Vanz <jeanfolle86@gmail.com>
2023-11-19 23:00:30 -0300

    Aulas 25 e 26
    Criação da documentação da API com o Swagger
    
    Adicionadas dependências springdoc ao pom.xml

--- src/main/java/br/edu/atitus/poo/atitusound/controllers/GenericController.java
+++ src/main/java/br/edu/atitus/poo/atitusound/controllers/GenericController.java
@@ -20,6 +20,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import br.edu.atitus.poo.atitusound.entities.GenericEntity;
 import br.edu.atitus.poo.atitusound.services.GenericService;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+@ApiResponses(value = {
+		@ApiResponse(responseCode = "400", description = "ERRO DE VALIDAÇÃO OU REQUISIÇÃO INVÁLIDA", 
+				content = @Content, headers = @Header(name = "error", description = "Descrição do erro", schema = @Schema(implementation = String.class))),
+		@ApiResponse(responseCode = "401", description = "UNAUTHORIZED", content = @Content),
+		@ApiResponse(responseCode = "403", description = "FORBIDDEN", content = @Content)
+	})
 
 public abstract class GenericController<TEntidade extends GenericEntity, TDTO> {
 	
@@ -28,6 +40,7 @@ public abstract class GenericController<TEntidade extends GenericEntity, TDTO> {
 	protected abstract TEntidade convertDTO2Entity(TDTO dto);
 
 	@PostMapping
+	@ApiResponse(responseCode = "201", description = "REGISTRO CRIADO COM SUCESSO")
 	public ResponseEntity<TEntidade> postSave(@RequestBody TDTO dto){
 		TEntidade entity = convertDTO2Entity(dto);
 		try {
@@ -39,6 +52,7 @@ public abstract class GenericController<TEntidade extends GenericEntity, TDTO> {
 	}
 	
 	@GetMapping
+	@ApiResponse(responseCode = "200", description = "OK")
 	public ResponseEntity<Page<List<TEntidade>>> getFind(
 			@PageableDefault(page = 0, size = 10, sort = "name", direction = Direction.ASC) Pageable pageable,
 			@RequestParam String name){
